da61cb46cbe50bb1d36c281d687e04a19a75993f
xogml123 <xogml951@gmail.com>
2022-11-18 17:32:55 +0900

    fix: build.gradle dependency in module-common common layer에서 web에 관련된 web, security, oauth배제 시킴

---
+++ module-batch/src/main/java/partner42/modulebatch/ModuleBatchApplication.java
@@ -0,0 +1,23 @@
+package partner42.modulebatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableBatchProcessing
+@EnableJpaAuditing
+@SpringBootApplication(
+    scanBasePackages = {"partner42.modulebatch", "partner42.modulecommon"}
+)
+@EntityScan(basePackages = "partner42.modulecommon.domain")
+@EnableJpaRepositories(basePackages =  "partner42.modulecommon.repository")
+public class ModuleBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleBatchApplication.class, args);
+    }
+
+}
