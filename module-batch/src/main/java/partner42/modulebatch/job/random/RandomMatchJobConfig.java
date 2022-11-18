4e2a2f4d9c329badbe79c6fc9b120073c553ecb4
xogml123 <xogml951@gmail.com>
2022-11-19 02:40:36 +0900

    fix: slackBotApi

--- module-batch/src/main/java/partner42/modulebatch/job/random/RandomMatchJobConfig.java
+++ module-batch/src/main/java/partner42/modulebatch/job/random/RandomMatchJobConfig.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,31 +20,33 @@ public class RandomMatchJobConfig {
     private final StepBuilderFactory stepBuilderFactory; // 생성자 DI 받음
 
     @Bean
-    public Job simpleJob() {
-        return jobBuilderFactory.get("simpleJob")
-            .start(simpleStep1())
+    public Job randomMatchJob() {
+        return jobBuilderFactory.get("randomMatchJob")
+            .start(expiringStep(null))
+            .next(simpleStep2(null))
             .build();
     }
 
-//    @Bean
-//    @JobScope
-//    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
-//        return stepBuilderFactory.get("simpleStep1")
-//            .tasklet((contribution, chunkContext) -> {
-//                log.info(">>>>> This is Step1");
-//                log.info(">>>>> requestDate = {}", requestDate);
-//                return RepeatStatus.FINISHED;
-//            })
-//            .build();
-//    }
+    @Bean
+    @JobScope
+    public Step expiringStep(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("expiringStep")
+            .tasklet((contribution, chunkContext) -> {
+                throw new IllegalArgumentException("step1에서 실패합니다.");
+            })
+            .build();
+    }
 
     @Bean
-    public Step simpleStep1() {
-        return stepBuilderFactory.get("simpleStep1")
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
             .tasklet((contribution, chunkContext) -> {
-                log.info(">>>>> This is Step1");
+                log.info(">>>>> This is Step2");
+                log.info(">>>>> requestDate = {}", requestDate);
                 return RepeatStatus.FINISHED;
             })
             .build();
     }
+
 }
