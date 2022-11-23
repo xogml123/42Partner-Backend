c5989576aa67406de247b5f1cbcfca5e9a55fca4
xogml123 <xogml951@gmail.com>
2022-11-24 01:25:43 +0900

    fix: AuthenticationPrincipal

--- module-common/src/main/java/partner42/modulecommon/repository/match/MatchRepositoryCustom.java
+++ module-common/src/main/java/partner42/modulecommon/repository/match/MatchRepositoryCustom.java
@@ -1,5 +1,6 @@
 package partner42.modulecommon.repository.match;
 
+import org.springframework.data.domain.SliceImpl;
 import partner42.modulecommon.domain.model.match.Match;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
