76e1cfce91984d338aa811f2ce8d8ac0d7c8a514
xogml123 <xogml951@gmail.com>
2022-11-29 16:52:58 +0900

    fix: 낙관적 락 발생시 재시도 횟수 3번으로 늘리고 0.1초 쉬게 만듬.

--- module-api/src/main/java/partner42/moduleapi/aop/OptimisticLockAspect.java
+++ module-api/src/main/java/partner42/moduleapi/aop/OptimisticLockAspect.java
@@ -1,5 +1,6 @@
 package partner42.moduleapi.aop;
 
+import javax.persistence.OptimisticLockException;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -16,17 +17,32 @@ import org.springframework.core.annotation.Order;
 @Order(99)
 @Aspect
 public class OptimisticLockAspect {
+    public static final Integer RETRY_MAX_COUNT = 3;
+    public static final Integer RETRY_WAIT_TIME = 100;
 
+    /**
+     * RETRY_MAX_COUNT만큼 반복하여 OptimisticLockException이 발생 하면 retry
+     * @param joinPoint
+     * @return
+     * @throws Throwable
+     */
     @Around("partner42.moduleapi.aop.PointCut.allPublicArticleService()")
     public Object doOneMoreRetryTransactionIfOptimisticLockExceptionThrow(
         ProceedingJoinPoint joinPoint) throws Throwable {
-        try{
-            return joinPoint.proceed();
-        } catch (Exception e) {
-            log.error("OptimisticLockException 발생");
-            return joinPoint.proceed();
-        } finally {
-            log.info("{}", joinPoint.getSignature());
+        Exception exceptionHolder = null;
+        for (int retryCount = 0; retryCount <= RETRY_MAX_COUNT; retryCount++) {
+            try {
+                log.info("[RETRY_COUNT]: {}", retryCount);
+                return joinPoint.proceed();
+            } catch (OptimisticLockException e) {
+                log.error("OptimisticLockException 발생");
+                exceptionHolder = e;
+                //RETRY_WAIT_TIME ms 쉬고 다시 시도
+                Thread.sleep(RETRY_WAIT_TIME);
+            }
         }
+        //3번 retry했음에도 exception이 발생하면 exception을 throw
+        throw exceptionHolder;
     }
+
 }
