cdbd08bd29559c792d083d7edfba7d7c532824f0
xogml123 <xogml951@gmail.com>
2023-03-03 13:32:50 +0900

    protype complete

--- module-common/src/main/java/partner42/modulecommon/config/jpa/JPAScanConfig.java
+++ module-common/src/main/java/partner42/modulecommon/config/jpa/JPAScanConfig.java
@@ -1,11 +1,10 @@
-package partner42.moduleapi.config.jpa;
+package partner42.modulecommon.config.jpa;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaAuditing
 @EnableJpaRepositories(basePackages =  "partner42.modulecommon.repository")
 @EntityScan(basePackages = "partner42.modulecommon.domain")
 @Configuration
--- module-common/src/test/java/partner42/modulecommon/ModuleCommonApplicationTests.java
+++ module-common/src/test/java/partner42/modulecommon/ModuleCommonApplicationTests.java
@@ -1,19 +1,14 @@
 package partner42.modulecommon;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import partner42.modulecommon.config.auditing.Auditor;
+import partner42.modulecommon.config.jpa.Auditor;
 import partner42.modulecommon.config.querydsl.QuerydslConfig;
 import partner42.modulecommon.utils.CreateTestDataUtils;
 
 
-@SpringBootTest
 @SpringBootApplication
-@EnableJpaAuditing
 @Import({CreateTestDataUtils.class, QuerydslConfig.class, Auditor.class})
 class ModuleCommonApplicationTests {
 
