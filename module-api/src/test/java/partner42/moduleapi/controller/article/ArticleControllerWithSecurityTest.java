2d33eb18bde7a1d9aca5a87e6c87af2a505320b5
xogml123 <xogml951@gmail.com>
2023-04-02 06:15:55 +0900

    fix: delete AuthenticationEntryPoint

--- module-api/src/test/java/partner42/moduleapi/controller/article/ArticleControllerWithSecurityTest.java
+++ module-api/src/test/java/partner42/moduleapi/controller/article/ArticleControllerWithSecurityTest.java
@@ -1,17 +1,16 @@
 package partner42.moduleapi.controller.article;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+
+import static org.mockito.BDDMockito.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -20,35 +19,35 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import partner42.moduleapi.config.security.CustomAuthorizationFilter;
 import partner42.moduleapi.config.security.SecurityConfig;
-import partner42.moduleapi.dto.article.ArticleReadOneResponse;
 import partner42.moduleapi.service.article.ArticleService;
-import partner42.moduleapi.service.user.CustomOAuth2UserService;
 import partner42.modulecommon.utils.slack.SlackBotService;
 
-//Spring Security설정 배제
 @WebMvcTest(value = {ArticleController.class},
     excludeFilters = {
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
     }
 )
-@AutoConfigureWebMvc
-class ArticleControllerTest {
+public class ArticleControllerWithSecurityTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private WebApplicationContext context;
-    @MockBean
-    private ArticleService articleService;
 
     @MockBean
+    private ArticleService articleService;
+    @MockBean
     private SlackBotService slackBotService;
+
+    @BeforeEach
+    private void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply(springSecurity())
+            .build();
+    }
 
     @Test
     @WithMockUser("username")
@@ -56,24 +55,22 @@ class ArticleControllerTest {
         //given
         String username = "username";
         String articleId = "articleId";
-        ArticleReadOneResponse articleReadOneResponse = ArticleReadOneResponse.builder()
-            .articleId(articleId)
-            .build();
-
-//        when(articleService.readOneArticle(any(), any())).thenReturn(articleReadOneResponse);
-//        when(slackBotService.createSlackMIIM(List.of())
-        //andExpect
+        //when
         mockMvc.perform(get("/api/articles/" + articleId))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.articleId").value(articleId))
-            .andDo(print());
-        //verify
-//        verify(articleService).readOneArticle(username, articleId);
+            .andDo(print())
+            .andExpect(status().isOk());
+        //then
+        verify(articleService).readOneArticle(username, articleId);
     }
-
     @Test
-//    @WithMockUser(authorities = "article.create")
-    void writeArticle() {
+    void readOneArticle_whenUserIsNotAuthenticated_thenVerify() throws Exception {
         //given
+        String articleId = "articleId";
+        //when
+        mockMvc.perform(get("/api/articles/" + articleId))
+            .andDo(print())
+            .andExpect(status().isOk());
+        //then
+        verify(articleService).readOneArticle(null, articleId);
     }
 }
