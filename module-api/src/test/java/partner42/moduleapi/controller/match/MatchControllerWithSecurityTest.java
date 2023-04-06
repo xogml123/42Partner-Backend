3653b6f8c7e7a411e95caa529781d2f718e7c41f
xogml123 <xogml951@gmail.com>
2023-04-06 12:15:37 +0900

    config: module-api, moddule-common build.gradle에jacoco test coverage verification  설정  완료

--- module-api/src/test/java/partner42/moduleapi/controller/match/MatchControllerWithSecurityTest.java
+++ module-api/src/test/java/partner42/moduleapi/controller/match/MatchControllerWithSecurityTest.java
@@ -1,6 +1,5 @@
 package partner42.moduleapi.controller.match;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -22,15 +21,9 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import partner42.moduleapi.annotation.WebMvcTestSecurityImport;
 import partner42.moduleapi.config.WebMvcTestWithSecurityDefaultConfig;
-import partner42.moduleapi.config.security.CustomAuthenticationEntryPoint;
-import partner42.moduleapi.config.security.RedirectAuthenticationFailureHandler;
-import partner42.moduleapi.config.security.RedirectAuthenticationSuccessHandler;
-import partner42.moduleapi.controller.alarm.AlarmController;
 import partner42.moduleapi.dto.match.MatchReviewRequest;
 import partner42.moduleapi.service.match.MatchService;
-import partner42.moduleapi.service.user.CustomOAuth2UserService;
 
 @WebMvcTest(MatchController.class)
 @Import(WebMvcTestWithSecurityDefaultConfig.class)
--- module-api/src/test/java/partner42/moduleapi/controller/random/RandomMatchControllerWithSecurityTest.java
+++ module-api/src/test/java/partner42/moduleapi/controller/random/RandomMatchControllerWithSecurityTest.java
@@ -1,6 +1,5 @@
 package partner42.moduleapi.controller.random;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -22,20 +21,13 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import partner42.moduleapi.annotation.WebMvcTestSecurityImport;
 import partner42.moduleapi.config.WebMvcTestWithSecurityDefaultConfig;
-import partner42.moduleapi.config.security.CustomAuthenticationEntryPoint;
-import partner42.moduleapi.config.security.RedirectAuthenticationFailureHandler;
-import partner42.moduleapi.config.security.RedirectAuthenticationSuccessHandler;
-import partner42.moduleapi.controller.opinion.OpinionController;
 import partner42.moduleapi.dto.matchcondition.MatchConditionRandomMatchDto;
-import partner42.moduleapi.dto.opinion.OpinionDto;
 import partner42.moduleapi.dto.random.RandomMatchCancelRequest;
 import partner42.moduleapi.dto.random.RandomMatchDto;
 import partner42.moduleapi.service.random.RandomMatchService;
 import partner42.modulecommon.domain.model.match.ContentCategory;
 import partner42.modulecommon.domain.model.matchcondition.TypeOfStudy;
-import partner42.modulecommon.domain.model.matchcondition.WayOfEating;
 
 @WebMvcTest(RandomMatchController.class)
 @Import(WebMvcTestWithSecurityDefaultConfig.class)
