407a10a7530c3f88f3367117707c3b6b3f04b9a9
xogml123 <xogml951@gmail.com>
2023-06-06 21:41:56 +0900

    for deploy

--- module-api/src/main/java/partner42/moduleapi/controller/HealthCheckController.java
+++ module-api/src/main/java/partner42/moduleapi/controller/HealthCheckController.java
@@ -27,7 +27,7 @@ public class HealthCheckController {
      */
     @GetMapping("/")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("test123811");
+        return ResponseEntity.ok("test1238111122");
     }
 
 //    @GetMapping("/health")
